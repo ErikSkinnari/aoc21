9d88a21ee7f2ebdcf59da2195e50eb6417b54bd3
Erik Skinnari <erik@skinnari.se>
2021-12-03 16:59:06 +0100

    start day 2

---
+++ src/Day2/Day2.cs
@@ -0,0 +1,25 @@
+using static aoc21.Helpers;
+
+namespace aoc21
+{
+
+  public class Day2
+  {
+    List<string> input;
+    
+    public Day2()
+    {
+      input = GetInput(@"C:\src\aoc21\src\Day2\input.txt").ToList();        
+    }
+
+    public void Run()
+    {
+      
+
+    }
+
+
+    
+
+  }
+}
